5743e84d0e891d87465b03f85a2ff74d52bd0818
Abdur Rahim <abdurrahim99100@gmail.com>
2024-01-30 23:05:57 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// html file chang javascript code index.html;
+document.getElementById('button-home').addEventListener('click', function () {
+    window.location.href = 'index.html'
+});
+
+// html file chang javascript code about.html;
+document.getElementById('button-about').addEventListener('click', function(){
+    window.location.href='about.html'
+});
+
+// html file chang javascript code services.html;
+document.getElementById('button-service').addEventListener('click', function(){
+    window.location.href='services.html'
+});
+
+// html file chang javascript code services.html;
+document.getElementById('button-contact').addEventListener('click', function(){
+    window.location.href='contact.html'
+});
+
+
